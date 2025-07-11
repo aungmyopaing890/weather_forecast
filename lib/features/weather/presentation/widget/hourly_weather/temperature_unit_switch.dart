0d03197e0a296ce845338e0c518d16e72ac7c036
aungmyopaing <62334147+aungmyopaing890@users.noreply.github.com>
2025-07-11 11:39:13 +0700

    TemperatureUnitSwitch error fix

--- lib/features/weather/presentation/widget/hourly_weather/temperature_unit_switch.dart
+++ lib/features/weather/presentation/widget/hourly_weather/temperature_unit_switch.dart
@@ -2,31 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_forecast/features/weather/data/enum/temperature_unit_enum.dart';
 import 'package:weather_forecast/features/weather/presentation/bloc/bloc/temperature_unit_cubit.dart';
-import 'package:weather_forecast/features/weather/presentation/bloc/state/hourly_weather_state.dart';
-import 'package:weather_forecast/features/weather/presentation/bloc/bloc/hourly_weather_bloc.dart';
 
 class TemperatureUnitSwitch extends StatelessWidget {
   const TemperatureUnitSwitch({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
+    return BlocBuilder<TemperatureUnitCubit, TemperatureUnit>(
+      builder: (context, unit) {
         return Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('°C'),
-            BlocBuilder<TemperatureUnitCubit, TemperatureUnit>(
-              builder: (context, unit) {
-                return Switch(
-                  value: context.select((HourlyWeatherBloc vm) =>
-                      vm.state is HourlyWeatherLoaded &&
-                      (vm.state as HourlyWeatherLoaded).unit ==
-                          TemperatureUnit.fahrenheit),
-                  onChanged: (_) {
-                    context.read<TemperatureUnitCubit>().toggleUnit();
-                  },
-                );
+            Switch(
+              value: unit == TemperatureUnit.fahrenheit,
+              onChanged: (_) {
+                context.read<TemperatureUnitCubit>().toggleUnit();
               },
             ),
             const Text('°F'),
