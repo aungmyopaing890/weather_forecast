ba06aa5b64a07feca3fd300dd0b6cd3a31db09cb
aungmyopaing <62334147+aungmyopaing890@users.noreply.github.com>
2025-07-11 11:45:52 +0700

    GenerateMocks

--- test/helper/test_helper.dart
+++ test/helper/test_helper.dart
@@ -1,8 +1,22 @@
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
+import 'package:weather_forecast/features/weather/domain/data_sources/weather_api_service.dart';
+import 'package:weather_forecast/features/weather/domain/repository/weather_repository.dart';
+import 'package:weather_forecast/features/weather/domain/usecases/fetch_hourly_weather_usecase.dart';
+import 'package:weather_forecast/features/weather/domain/usecases/fetch_weather_usecase.dart';
+import 'package:weather_forecast/features/weather/presentation/bloc/bloc/hourly_weather_bloc.dart';
+import 'package:weather_forecast/features/weather/presentation/bloc/bloc/temperature_unit_cubit.dart';
+import 'package:weather_forecast/features/weather/presentation/bloc/bloc/weather_bloc.dart';
 
 @GenerateMocks(
   [
+    WeatherApiService,
+    FetchWeatherUseCase,
+    FetchHourlyWeatherUseCase,
+    WeatherRepository,
+    HourlyWeatherBloc,
+    TemperatureUnitCubit,
+    WeatherBloc,
     http.Client,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
